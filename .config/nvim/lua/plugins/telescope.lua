4041ff9fce8e9982b11acf32087fc72d58684625
Philip Schleiter <phossc@protonmail.com>
2023-02-20 00:07:53 +0100

    Setup telescope and leap

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = 'Telescope',
+    keys = {
+      { '<Leader>;', '<Cmd>Telescope live_grep<CR>' },
+      { '<Leader>/', '<Cmd>Telescope current_buffer_fuzzy_find<CR>' },
+      { '<Leader>f', '<Cmd>Telescope find_files<CR>' },
+      { '<Leader>gf', '<Cmd>Telescope git_files<CR>' },
+      { '<Leader>gb', '<Cmd>Telescope git_branches<CR>' },
+      { '<Leader>gs', '<Cmd>Telescope git_status<CR>' },
+      { '<Leader>gc', '<Cmd>Telescope git_commits<CR>' },
+    },
+  }
+}
+
