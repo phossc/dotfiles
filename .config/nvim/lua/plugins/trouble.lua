ef0471b9af32689d91d65fe3540c3dfb800494c5
Philip Schleiter <phossc@protonmail.com>
2023-08-26 20:39:19 +0200

    feat: setup trouble.nvim

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
+  keys = {
+    { "<Leader>xx", "<Cmd>TroubleToggle document_diagnostics<CR>" },
+  },
+}
