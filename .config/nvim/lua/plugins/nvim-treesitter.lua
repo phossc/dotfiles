cfb4ff9be84e92d01852d3b9599e5f11a67b7dab
Philip Schleiter <phossc@protonmail.com>
2023-12-31 20:16:47 +0100

    fix: add required treesitter languages

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = { ensure_installed = { "cpp", "c" } },
+    opts = { ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp" } },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
