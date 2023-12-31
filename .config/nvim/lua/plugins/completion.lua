a5dfb4b76d06c79720b270d5cc0a9f76d381a5fb
Philip Schleiter <phossc@protonmail.com>
2023-12-31 19:08:18 +0100

    feat: remove snippet plugins

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,32 +1,16 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     version = false,
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
       "github/copilot.vim",
     },
     opts = function()
       local cmp = require("cmp")
-      local luasnip = require("luasnip")
       return {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
         mapping = cmp.mapping.preset.insert({
           ["<C-e>"] = cmp.mapping.abort(),
           ["<C-u>"] = cmp.mapping.scroll_docs(-4),
@@ -52,17 +36,8 @@ return {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() and cmp.get_selected_entry() then
               cmp.confirm()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
             elseif cmp.visible() then
               cmp.confirm({ select = true })
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
             else
               fallback()
             end
@@ -91,7 +66,6 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" },
           { name = "buffer" },
           { name = "path" },
         }),
