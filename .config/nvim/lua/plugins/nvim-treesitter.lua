4d48c686a2e5ad349184a478372e41b173d203c7
Philip Schleiter <phossc@protonmail.com>
2025-02-19 20:53:44 +0100

    chore(neovim): add zig tree-sitter grammar

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = { ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp" } },
+    opts = { ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp", "zig" } },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
