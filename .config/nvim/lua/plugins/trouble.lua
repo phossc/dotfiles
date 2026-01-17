c85c846ceb507ce245b2be0c4403f0fe3da54b0d
Philip Schleiter <phossc@protonmail.com>
2026-01-17 15:16:13 +0100

    neovim: make it plugin free

--- .config/nvim/lua/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  keys = {
-    { "<Leader>xx", "<Cmd>TroubleToggle document_diagnostics<CR>" },
-  },
-}
--- .config/nvim/lua/plugins/vim-illuminate.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "RRethy/vim-illuminate",
-    opts = {},
-    config = function(_, opts)
-      require("illuminate").configure(opts)
-    end,
-  },
-}
