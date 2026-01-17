7478fbc093818e0aab72cb3364c85ae8fa5ca343
Philip Schleiter <phossc@protonmail.com>
2026-01-17 16:43:27 +0100

    setup: don't clone tmux plugin manager

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/tmux-plugins/tpm ~/.local/share/tmux/plugins/tpm
-
 ln -s "$(brew --prefix llvm)/bin/clang-tidy" "/usr/local/bin/clang-tidy"
 ln -s "$(brew --prefix llvm)/bin/clang-apply-replacements" "/usr/local/bin/clang-apply-replacements"
